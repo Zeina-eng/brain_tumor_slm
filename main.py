65d737e42af2b0a904f03d8cf1b8593366331632
Zeina-eng <zenamansour512@gmail.com>
2026-03-26 23:34:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
 import requests
 from docx import Document
 import io
@@ -6,9 +7,19 @@ import os
 
 app = FastAPI()
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # change to your Vercel URL later
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/")
 def home():
-    return {"message": "AI Paper Summarizer"}
+    return {"message": "AI Paper Summarizer running 🚀"}
+
 
 @app.post("/upload/")
 async def upload_file(file: UploadFile = File(...)):
@@ -16,11 +27,20 @@ async def upload_file(file: UploadFile = File(...)):
         API_URL = "https://router.huggingface.co/hf-inference/models/google/flan-t5-small"
 
         HF_TOKEN = os.getenv("HF_TOKEN")
+
+        
+        if not HF_TOKEN:
+            return {"error": "HF_TOKEN not set in environment variables"}
+
         HEADERS = {"Authorization": f"Bearer {HF_TOKEN}"}
 
         content = await file.read()
 
-        # Handle file types
+        
+        if len(content) > 1_000_000:
+            return {"error": "File too large (max 1MB)"}
+
+        
         if file.filename.endswith(".txt"):
             text = content.decode("utf-8")
 
@@ -31,9 +51,24 @@ async def upload_file(file: UploadFile = File(...)):
         else:
             return {"error": "Only .txt and .docx supported"}
 
-        response = requests.post(API_URL, headers=HEADERS, json={"inputs": text})
+       
+        text = text[:2000]
 
-        return response.json()
+        
+        response = requests.post(
+            API_URL,
+            headers=HEADERS,
+            json={"inputs": f"Summarize this:\n{text}"},
+            timeout=30
+        )
+
+        result = response.json()
+
+        
+        if isinstance(result, list) and "generated_text" in result[0]:
+            return {"summary": result[0]["generated_text"]}
+
+        return {"raw_response": result}
 
     except Exception as e:
         return {"error": str(e)}
